0367c4148c23bca39f5ff568cf4282d742d28de2
Ana Mrnngstr <tyurinaana666@gmail.com>
2023-03-12 15:12:49 +0300

    All three tasks finale

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,19 +1,19 @@
 const menuLink = document.getElementsByClassName('menu__link');
 
-
-
 for (let el = 0; el < menuLink.length; el++) {
 
 const menu = menuLink[el];
 const sub = menu.closest('.menu__item').querySelector('.menu_sub');
+
 menu.onclick = () => {
 
     if (sub) {
-        sub.classList.add('menu_active');
+
+        sub.classList.toggle('menu_active');
         return false;
         }
     }
-    
+
 }
 
 
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,7 +1,6 @@
 let sliderItem = Array.from(document.querySelectorAll(".slider__item"));
 let rightClick = document.querySelector('.slider__arrow_next');
 let leftClick = document.querySelector('.slider__arrow_prev');
-let currentPage = 0;
 
 const remove = () => {
     sliderItem.forEach(el => el.classList.remove('slider__item_active'));
@@ -23,7 +22,7 @@ rightClick.onclick = () => {
 leftClick.onclick = () => {
     let active = sliderItem.findIndex(el => el.classList.contains('slider__item_active'))
     if (active <= 0) {
-        active = sliderItem.length;
+        active = sliderItem.length - 1;
     }
     remove();
     add(active - 1);
