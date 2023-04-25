8f12fad30069387394aa61d8c402ea6dc805b6a9
Ana Mrnngstr <tyurinaana666@gmail.com>
2023-04-26 01:05:17 +0300

    3 tasks

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,4 +1,13 @@
 const progress = document.getElementById( 'progress' );
-const xhr = new XMLHttpRequest();
-xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
-xhr.send();
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let form = new FormData();
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+    xhr.upload.onprogress = (event) => {
+        progress.value = event.loaded / event.total;
+    }
+    xhr.send(form);
+})
