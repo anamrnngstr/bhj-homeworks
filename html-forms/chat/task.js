2f8e8169c7e68ef9b10fbc2a7d9c3fbb1c56cd6e
Ana Mrnngstr <tyurinaana666@gmail.com>
2023-05-01 00:32:49 +0300

    edited

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -5,11 +5,28 @@ const input = document.querySelector('.chat-widget__input');
 chat.addEventListener('click', () => {
     chat.classList.add('chat-widget_active');
 })
+let messageText = [
+    'Чем могу помочь?',
+    'Добрый день!',
+    'Хорошего дня',
+    'Спасибо',
+    'До досвидания',
+    'Отлично!',
+    'К сожалению все операторы заняты',
+    'Обратитесь в службу поддержки'
+];
+let rand = Math.floor(Math.random() * messageText.length)
+let messageRandom = messageText[rand];
 
 document.addEventListener('keyup', (e) => {
     if (e.code === 'Enter') {
-        messages.innerHTML += '<div class="message"><div class="message__time">22:10</div><div class="message__text">Добрый день!</div></div>'
-        let message = document.querySelector('.message__text');
-        message.innerHTML = input.value;
+        messages.innerHTML += `<div class="message message_client"><div class="message__time">22:10</div><div class="message__text">${input.value}</div></div>`
+        messages.innerHTML += `<div class="message">
+        <div class="message__time">22:10</div>
+        <div class="message__text">
+            ${messageRandom}
+        </div>
+    </div>`
+
     }
 })
