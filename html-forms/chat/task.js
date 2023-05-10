57fc4abca53525ab1cd0bb4c7742693801a98423
Ana Mrnngstr <tyurinaana666@gmail.com>
2023-05-10 23:03:43 +0300

    edited chat

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,6 +1,7 @@
 const chat = document.querySelector('.chat-widget');
 const messages = document.querySelector( '.chat-widget__messages' );
 const input = document.querySelector('.chat-widget__input');
+let nowTime = new Date().toLocaleTimeString().slice(0, -3);
 
 chat.addEventListener('click', () => {
     chat.classList.add('chat-widget_active');
@@ -20,13 +21,22 @@ let messageRandom = messageText[rand];
 
 document.addEventListener('keyup', (e) => {
     if (e.code === 'Enter') {
-        messages.innerHTML += `<div class="message message_client"><div class="message__time">22:10</div><div class="message__text">${input.value}</div></div>`
-        messages.innerHTML += `<div class="message">
-        <div class="message__time">22:10</div>
-        <div class="message__text">
-            ${messageRandom}
-        </div>
-    </div>`
+        if (input.value) {
+            messages.innerHTML += `
+                    <div class="message message_client">
+                        <div class="message__time">${nowTime}</div>
+                        <div class="message__text">${input.value}</div>
+                    </div>
+                    `;
+				input.value = "";
+                messages.innerHTML += `<div class="message">
+                <div class="message__time">${nowTime}</div>
+                <div class="message__text">
+                    ${messageRandom}
+                </div>
+            </div>`
+        }
+    
 
     }
 })
